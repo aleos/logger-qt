60cc92b8c6ad55172e6bbb3be343ec9a8782c0db
aleos <aleos@localhost>
2011-07-05 15:13:23 +0400

    Use QTextStream

--- logger.cpp
+++ logger.cpp
@@ -1,82 +1,64 @@
-/*! \file    logger.h
+/*! \file    logger.cpp
     \author  aleos <aleos@flightstudio.ru>
 */
 
 #include "logger.h"
-#include <iostream>
 
-Logger* Logger::m_logger = 0;
-
-/////////////////////////////////////////////////////////////////////////////
+Logger* Logger::logger = 0;
 
 Logger::Logger()
 {
-    m_logfile = 0;
-    m_logfilestream = 0;
+    file = 0;
+    filestream = 0;
 }
-
-/////////////////////////////////////////////////////////////////////////////
 
 Logger::~Logger() 
 {
-    delete m_logfilestream;
-    m_logfilestream = 0;
-    m_logfile->flush();
-    delete m_logfile;
-    m_logfile = 0;
+    delete filestream;
+    filestream = 0;
+    file->flush();
+    delete file;
+    file = 0;
 }
-
-/////////////////////////////////////////////////////////////////////////////
 
 void Logger::setLogFile(const QString& logfilename)
 {
-//    MYASSERT(!logfilename.isEmpty());
-    if (m_logfile != 0) delete m_logfile;
-    if (m_logfilestream != 0) delete m_logfilestream;
+    if (file != 0) delete file;
+    if (filestream != 0) delete filestream;
     
-    m_logfile = new QFile(logfilename);
-//    MYASSERT(m_logfile != 0);
-    if (!m_logfile->open(QIODevice::WriteOnly))
+    file = new QFile(logfilename);
+    if (!file->open(QIODevice::WriteOnly))
     {
-#if! VASFMC_GAUGE
-//        QMessageBox::critical(0, "LOGFILE", QString("Could not open logfile (%1)").arg(logfilename));
-//        MYASSERT(false);
-#endif
+        qDebug() << "File not opened!";
     }
-//    MYASSERT(m_logfile->isWritable());
-//    MYASSERT(m_logfile->resize(0));
-    
-    m_logfilestream = new QTextStream(m_logfile);
-//    MYASSERT(m_logfilestream != 0);
+
+    file->resize(0);
+    filestream = new QTextStream(file);
 }
 
-/////////////////////////////////////////////////////////////////////////////
-
-void Logger::logText(const QString& text,bool console_only)
+void Logger::logText(const QString& text, bool console_only)
 {
-    QString logtext = QString("%1: %2").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss:zzz")).arg(text);
-    
-    // write out the text
-//    printf("%s\n", logtext.toLatin1().data());
-    QTextStream logToConsole(stdout);
-    logToConsole << logtext << "\n";
+//    QString logtext = QString("%1: %2").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss:zzz")).arg(text);
+    QString logtext = QString("%1: %2").arg(QDateTime::currentDateTime().toString("mm:ss:zzz")).arg(text);
 
-    fflush(stdout);
+    QTextStream terminal(stdout);
+    terminal << logtext << endl;
+//    terminal.flush();
+//    fflush(stdout);
     
     // write the text to the logfile
-    if (!console_only && m_logfilestream != 0) *m_logfilestream << logtext << endl;
+    if (!console_only && filestream != 0) *filestream << logtext << endl;
+//    filestream->flush();
     
     emit signalLogging(logtext);
 }
-
-/////////////////////////////////////////////////////////////////////////////
 
 void Logger::logTextToFileOnly(const QString& text)
 {
     QString logtext = QString("%1: %2").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss:zzz")).arg(text);
     
     // write the text to the logfile
-    if (m_logfilestream != 0) *m_logfilestream << logtext << endl;
+    if (filestream != 0) *filestream << logtext << endl;
 }
 
 // End of file

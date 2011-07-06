6a609698806b7d48f7703889c46e856e71539d3c
Alexander Ostrovsky <thealeos@gmail.com>
2011-07-06 18:28:41 +0400

    logger singleton

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <QtCore/QCoreApplication>
 #include <QTextCodec>
+
 #include "logger.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QTextCodec::setCodecForTr(QTextCodec::codecForName("Windows-1251"));
 
-    Logger logger;
-    logger.setLogFile("Logger.log");
-    logger.logText("sdfsdfsdfsdfsdfs");
+//    Logger::addLogFile(1, "vasya.txt");
+    Logger::log("hi");
+    Logger::log("hello");
+    Logger::log("end");
+
+
+
+//    std::map<int, std::ofstream*> m;
+//    m.insert(std::pair<int, std::ofstream*>(0, new std::ofstream("0")));
+//    m.insert(std::pair<int, std::ofstream*>(1, new std::ofstream("1")));
+
+//    std::map<int, std::ofstream*>::iterator iter = m.begin();
+//    iter = m.find(1);
+//    *iter->second << "lskjdf";
+//    iter->second->close();
+
+    std::ofstream f("qwe.txt");
+    f << "hi" << "\n";
+    f.flush();
+    f << "hello" << "\n";
+    f.flush();
+
 
     return a.exec();
 }
